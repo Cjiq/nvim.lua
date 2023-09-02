270a5fa075f4c8edf23a81d2c155e9fcd3566970
Erik Lundmark <cjiqqq@gmail.com>
2023-09-02 21:52:22 +0200

    Add diffview binding

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,1 +1,10 @@
 require('diffview').setup()
+
+local wk = require("which-key")
+wk.register({
+  g = {
+    d = { "<cmd>DiffviewOpen<cr>", "Git diff" },
+  },
+}, { prefix = "<leader>", mode = {"n"} })
+
+
