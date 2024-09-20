67729d8f688c60cbc47db0ab4c2cf0ead3e518d5
Erik Lundmark <cjiqqq@gmail.com>
2024-09-20 21:53:55 +0200

    add remaps for window control

--- lua/cjiq/remap.lua
+++ lua/cjiq/remap.lua
@@ -8,6 +8,11 @@ vim.keymap.set("n", "<leader>x", vim.cmd.hsp)
 vim.keymap.set("n", "<C-i>", vim.cmd.bprev)
 vim.keymap.set("n", "<C-o>", vim.cmd.bnext)
 
+vim.keymap.set("n", "<leader>wj", ":resize -5<cr>")
+vim.keymap.set("n", "<leader>wk", ":resize +5<cr>")
+vim.keymap.set("n", "<leader>w-", ":sp<cr>")
+vim.keymap.set("n", "<leader>w.", ":vsp<cr>")
+
 -- Nice move selected text up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -17,3 +22,6 @@ vim.keymap.set("x", "<leader>p", '"_dP')
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
+-- Map Esc in terminal mode to go back to normal mode
+vim.api.nvim_set_keymap("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
