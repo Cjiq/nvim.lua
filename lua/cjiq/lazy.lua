0c24eefef8461fa4a53165d49b13941ee27106c7
Erik Lundmark <erik.a.lundmark@skekraft.se>
2024-09-24 07:04:54 +0200

    fix go signature inline

--- lua/cjiq/lazy.lua
+++ lua/cjiq/lazy.lua
@@ -498,8 +498,13 @@ require("lazy").setup({
   },
   {
     "ray-x/lsp_signature.nvim",
-    event = "VeryLazy",
-    opts = {},
+    event = "InsertEnter",
+    opts = {
+      bind = true,
+      handler_opts = {
+        border = "rounded",
+      },
+    },
     config = function(_, opts)
       require("lsp_signature").setup(opts)
     end,
